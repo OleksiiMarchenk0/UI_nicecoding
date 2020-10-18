df3335a8b3cf7d573e503d0b52ba6ac25b9df181
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2020-10-18 22:13:44 +0200

    Added font 'Sen'

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll",function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY>0)
 });
+
+function scrollFunction1() {
+    let e = document.getElementById("team");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'start'
+    });
+}

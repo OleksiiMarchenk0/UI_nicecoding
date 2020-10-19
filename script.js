d5794176bcc9afc0704f203fc14d9e8e4d3f0d26
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2020-10-20 00:42:23 +0200

    Added popup demo

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ function scrollFunction1() {
       inline: 'start'
     });
 }
+
+//-------------
+let stateCheck = setInterval(() => {
+  if (document.readyState === 'complete') {
+    clearInterval(stateCheck);
+    const popup = document.querySelector(".popup");
+    const container = document.querySelector(".container");
+    var closeButton = document.querySelector(".popup__close");
+    closeButton.addEventListener("click",function(){
+      console.log(closeButton);
+    popup.style.display="none";
+    container.style="opacity:1;position:relative;"
+}, 100);
+}
+});
+function handleClick(memberId) { 
+  var popup;
+  const container = document.querySelector(".container");
+  if(memberId ==="member-1")
+  {
+    popup = document.getElementById("popup-1")
+  }
+  else if(memberId ==="member-2")
+  {
+    popup = document.getElementById("popup-2")
+  }
+  else if(memberId ==="member-3")
+  {
+    popup = document.getElementById("popup-3")
+  }
+  else
+  {
+    popup = document.getElementById("popup-4")
+  }
+  var member = document.getElementById(memberId);
+    popup.style.display="flex";
+   // container.style="opacity:.3;"
+}

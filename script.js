696cbb251a8467a9d4f3755c9d7e43e94f45763e
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2020-10-21 00:29:10 +0200

    Changes

--- script.js
+++ script.js
@@ -1,51 +1,46 @@
-window.addEventListener("scroll",function(){
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky",window.scrollY>0)
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  // var popupContainer = document.getElementById('popupContainer'),
+  //   style = window.getComputedStyle(popupContainer),
+  //   display = style.getPropertyValue('display');
+  // if(display==="none")
+//  {
+    header.classList.toggle("sticky", window.scrollY > 0);
+  //}
+  
 });
 
 function scrollFunction1() {
-    let e = document.getElementById("team");
-    e.scrollIntoView({
-      block: 'start',
-      behavior: 'smooth',
-      inline: 'start'
-    });
+  let e = document.getElementById("team");
+  e.scrollIntoView({
+    block: "start",
+    behavior: "smooth",
+    inline: "start",
+  });
 }
 
 //-------------
 let stateCheck = setInterval(() => {
-  if (document.readyState === 'complete') {
+  if (document.readyState === "complete") {
     clearInterval(stateCheck);
-    const popup = document.querySelector(".popup");
-    const container = document.querySelector(".container");
-    var closeButton = document.querySelector(".popup__close");
-    closeButton.addEventListener("click",function(){
-      console.log(closeButton);
-    popup.style.display="none";
-    container.style="opacity:1;position:relative;"
-}, 100);
-}
+    //
+    var containerHeight = document.getElementById("container").clientHeight;
+    var popupContainer = document.getElementById("popupContainer");
+    popupContainer.style.height = containerHeight + "px";
+  }
 });
-function handleClick(memberId) { 
+function handleClick(memberId) {
   var popup;
-  const container = document.querySelector(".container");
-  if(memberId ==="member-1")
-  {
-    popup = document.getElementById("popup-1")
+  var popupContainer = document.getElementById("popupContainer");
+  if (memberId === "member-1") {
+    popup = document.getElementById("popup-1");
+  } else if (memberId === "member-2") {
+    popup = document.getElementById("popup-2");
+  } else if (memberId === "member-3") {
+    popup = document.getElementById("popup-3");
+  } else {
+    popup = document.getElementById("popup-4");
   }
-  else if(memberId ==="member-2")
-  {
-    popup = document.getElementById("popup-2")
-  }
-  else if(memberId ==="member-3")
-  {
-    popup = document.getElementById("popup-3")
-  }
-  else
-  {
-    popup = document.getElementById("popup-4")
-  }
-  var member = document.getElementById(memberId);
-    popup.style.display="flex";
-   // container.style="opacity:.3;"
+  popupContainer.style.display = "flex";
+  popup.style.display = "flex";
 }

515c9e7ec694e34f32a1d9389b2b073bc7ff90f3
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2020-10-21 22:07:27 +0200

    Naprawiono overflow-x

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
-  // var popupContainer = document.getElementById('popupContainer'),
-  //   style = window.getComputedStyle(popupContainer),
-  //   display = style.getPropertyValue('display');
-  // if(display==="none")
-//  {
     header.classList.toggle("sticky", window.scrollY > 0);
-  //}
-  
 });
-
 function scrollFunction1() {
   let e = document.getElementById("team");
   e.scrollIntoView({
@@ -18,7 +10,6 @@ function scrollFunction1() {
     inline: "start",
   });
 }
-
 //-------------
 let stateCheck = setInterval(() => {
   if (document.readyState === "complete") {
@@ -30,8 +21,10 @@ let stateCheck = setInterval(() => {
   }
 });
 function handleClick(memberId) {
+  console.log(memberId)
   var popup;
   var popupContainer = document.getElementById("popupContainer");
+  var container = document.getElementById("container");
   if (memberId === "member-1") {
     popup = document.getElementById("popup-1");
   } else if (memberId === "member-2") {
@@ -42,5 +35,22 @@ function handleClick(memberId) {
     popup = document.getElementById("popup-4");
   }
   popupContainer.style.display = "flex";
-  popup.style.display = "flex";
+  popup.style.cssText += 'display: flex;';
+  container.style.position = "fixed";
+}
+function closePopup(btnId){
+  var popupContainer = document.getElementById("popupContainer");
+  var container = document.getElementById("container");
+  if (btnId === "btn-1") {
+    popup = document.getElementById("popup-1");
+  } else if (btnId === "btn-2") {
+    popup = document.getElementById("popup-2");
+  } else if (btnId === "btn-3") {
+    popup = document.getElementById("popup-3");
+  } else {
+    popup = document.getElementById("popup-4");
+  }
+  popup.style.cssText += 'display: none;';
+  popupContainer.style.display = "none";  
+  container.style.position = "relative";
 }
